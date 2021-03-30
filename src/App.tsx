f988aa983bfa151201296d1c874a9a5e3e98500f
michaeli10m <michaeli10m@yahoo.com>
2021-03-30 16:36:16 +0300

    Test commit from Michael

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <Login name="michael_me" />
+                
             </header>
         </div>
     );
